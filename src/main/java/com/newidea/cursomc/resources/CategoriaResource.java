860bd4ceed487f24cde820507f866f2a9c2f1c15
Ronaldo Castilho <ronaldolcastilho@gmail.com>
2019-09-15 07:52:26 -0300

    teste rest

---
+++ src/main/java/com/newidea/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.newidea.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categoriaa")
+public class CategoriaResource {
+
+    @GetMapping
+    public String listar(){
+        return "lista vazia";
+    }
+}

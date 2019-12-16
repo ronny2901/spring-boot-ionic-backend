c8058c7a3d692fb00668e81870a06d21bae0525b
Ronaldo Castilho <ronaldolcastilho@gmail.com>
2019-12-15 23:20:56 -0300

    h2 e criacao da base de dados automatica

--- src/main/java/com/newidea/cursomc/domain/Categoria.java
+++ src/main/java/com/newidea/cursomc/domain/Categoria.java
@@ -2,14 +2,21 @@ package com.newidea.cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 }

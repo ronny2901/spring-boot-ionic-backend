0106374694df13bfa794c1e52753cfe35ddfe9c6
Ronaldo Castilho <ronaldolcastilho@gmail.com>
2020-05-15 18:43:08 -0300

    add. categorias

--- src/main/java/com/newidea/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/newidea/cursomc/resources/CategoriaResource.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -32,5 +34,12 @@ public class CategoriaResource {
         Categoria obj = service.find(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> insert(Categoria obj){
+        obj= service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id]").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/newidea/cursomc/services/CategoriaService.java
+++ src/main/java/com/newidea/cursomc/services/CategoriaService.java
@@ -20,4 +20,11 @@ public class CategoriaService {
 
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: "+id+", Tipo: "+Categoria.class.getName()));
     }
+
+    public Categoria insert(Categoria obj){
+
+        obj.setId(null);
+
+        return respository.save(obj);
+    }
 }
